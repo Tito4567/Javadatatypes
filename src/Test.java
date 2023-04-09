7a78453a846e7d59b6dcac9d4a7e1f8de8ddf153
Gilberto Rose <gilbertorosester@gmail.com>
2023-04-09 14:05:01 -0400

    added the text file and modified src code

--- src/Test.java
+++ src/Test.java
@@ -7,12 +7,16 @@ import java.util.regex.*;
 public class Test {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String testCases = br.readLine();
-        int N = Integer.parseInt(testCases);
-        for (int i = 0; i < N; i++) {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String testCases = br.readLine();
+//        int N = Integer.parseInt(testCases);
+    	
+   /*added now*/ 	File fle = new File ("Data.txt");
+   /*added now*/ 	Scanner scn = new Scanner(fle);
+        for (int i = 0; i < 20; i++) {
             //BufferedReader brr = new BufferedReader(new InputStreamReader(System.in));
-            String line = br.readLine();
+      //      String line = br.readLine();
+        	String line = scn.nextLine();
             try {
                 long n = Long.parseLong(line);
                 if (n == 0 || n == 1) {
@@ -24,7 +28,7 @@ public class Test {
                 System.out.println("* long");
                 continue;
             } else if (n == (byte)n) {
-                System.out.println(n + " can be fitted in:");
+            	System.out.println(n + " can be fitted in:");
                 System.out.println("* byte");
                 System.out.println("* short");
                 System.out.println("* int");
@@ -32,18 +36,18 @@ public class Test {
                 continue;
             }
               else if (n == (short)n) {
-                System.out.println(n + " can be fitted in:");
+            	System.out.println(n + " can be fitted in:");
                 System.out.println("* short");
                 System.out.println("* int");
                 System.out.println("* long");
                 continue;
             } else if (n == (int)n) {
-                System.out.println(n + " can be fitted in:");
+            	System.out.println(n + " can be fitted in:");
                 System.out.println("* int");
                 System.out.println("* long");
                 continue;
             } else {
-                System.out.println(n + " can be fitted in:");
+            	System.out.println(n + " can be fitted in:");
                 System.out.println("* long");
                 continue;
            }
